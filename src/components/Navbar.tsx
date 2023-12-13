72587ccd68fc57c429731fb187d98b73297d7f38
Siddhant Yugal Daryanani <siddhantdaryanani@gmail.com>
2023-12-13 14:06:22 +0530

    Added reponsive shopping cart with interactive elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = ()=>{
     const user = null;
@@ -29,6 +30,9 @@ const Navbar = ()=>{
                                     {user ? null : <span className="h-6 w-px bg-gray-200" aria-hidden="true"/>}
                                     {user ? <p> </p> : <Link href='sign-up' className={buttonVariants({variant:'ghost'})}>Sign up</Link>}
                                     {user ? null : <div className="flex lg: ml-6">{user ? null : <span className="h-6 w-px bg-gray-200" aria-hidden="true"/>}</div>}
+                                    <div className="ml-4 flow-root lg:ml-6">
+                                        <Cart />
+                                    </div>
                                 </div>
                             </div>
                         </div>
