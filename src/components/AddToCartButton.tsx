8cd83c6e73f85720fed6ea648dca03f2368295db
Siddhant Yugal Daryanani <siddhantdaryanani@gmail.com>
2024-01-10 11:08:03 +0530

    Added persistent cart logic that works on the front end

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,8 +1,11 @@
 "use client"
 import { useEffect, useState } from "react"
 import { Button } from "./ui/button"
+import { useCart } from "@/hooks/use-cart"
+import { Product } from "@/payload-types"
 
-const AddToCartButton = () => {
+const AddToCartButton = ({product}:{product: Product}) => {
+    const {addItem} = useCart()
     const [isSuccess,setIsSuccess] = useState<boolean>(false)
 
     useEffect(()=> {
@@ -13,7 +16,8 @@ const AddToCartButton = () => {
         return () => clearTimeout(timeout)
     },[isSuccess])
 
-    return <Button onClick={()=> {setIsSuccess(true)}} size='lg' className="w-full">{isSuccess ? "Added!" : "Add to cart"}</Button>
+    return <Button onClick={()=> { addItem(product) 
+        setIsSuccess(true)}} size='lg' className="w-full">{isSuccess ? "Added!" : "Add to cart"}</Button>
 }
 
 
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -7,26 +7,43 @@ import { formatPrice } from '@/lib/utils';
 import Link from 'next/link';
 import Image from 'next/image';
 import { buttonVariants } from './ui/button';
+import { useCart } from '@/hooks/use-cart';
+import { ScrollArea } from '@radix-ui/react-scroll-area';
+import CartItem from './CartItem';
+import { useEffect, useState } from 'react';
 
 const Cart = () =>{
-    const itemCount = 0
+    const {items} = useCart()
+    const itemCount = items.length
+
+    const [isMounted, setIsMounted] = useState<boolean>(false)
+    useEffect(()=> {
+        setIsMounted(true)
+    },[])
+
+
+    const cartTotal = items.reduce((total,{product})=> total + product.price,0)
     const fee = 100;
     return (
         <Sheet>
             <SheetTrigger className='group -m-2 flex items-center p-2'>
                 <ShoppingCart className='h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'/>
                 <span className='ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800'>
-                    0
+                    {isMounted? itemCount : 0}
                 </span>
             </SheetTrigger>
             <SheetContent className='flex w-full flex-col sm:max-w-lg'>
                 <SheetHeader className='space-y-2.5 pr-6'>
-                    <SheetTitle>Cart(0)</SheetTitle>
+                    <SheetTitle>Cart({itemCount})</SheetTitle>
                 </SheetHeader>
                 {itemCount > 0 ? (
                     <>
                         <div className='flex w-full flex-col pr-6'>
-                            cart items
+                            <ScrollArea>
+                            {items.map(({product})=> (
+                                <CartItem product={product} key={product.id} />
+                            ))}
+                            </ScrollArea>
                         </div>
                         <div className='space-y-4 pr-6'>
                             <Separator />
@@ -41,7 +58,7 @@ const Cart = () =>{
                                 </div>
                                 <div className="flex">
                                     <span className="flex-1">Total</span>
-                                    <span>{formatPrice(fee)}</span>
+                                    <span>{formatPrice(cartTotal+ fee)}</span>
                                 </div>     
                             </div>
                             <SheetFooter>
